c14e643b04bf0a1980a0bf4df545fa077ca94837
workspace-rc <workspace@fundacionsomos.ar>
2026-05-04 09:52:11 -0300

    Update app_navegacion_3.py

--- app_navegacion_3.py
+++ app_navegacion_3.py
@@ -163,7 +163,8 @@ def main():
 
                 st.pydeck_chart(pdk.Deck(
                     # MAPA SATELITAL
-                    map_style="mapbox://styles/mapbox/light-v9",
+                    map_provider="carto",
+                    map_style="light", 
                     initial_view_state=view_state,
                     layers=[capa_ruta, capa_puntos],
                     tooltip={
